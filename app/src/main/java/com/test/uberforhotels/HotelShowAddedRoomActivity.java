57843047c8e5ba4bb2583e7746fa48452acd88b8
Hasham <hasham.khattak.hk@gmail.com>
2020-05-14 18:48:04 +0500

    ShowAddedRooms

--- app/src/main/java/com/test/uberforhotels/HotelShowAddedRoomActivity.java
+++ app/src/main/java/com/test/uberforhotels/HotelShowAddedRoomActivity.java
@@ -1,14 +1,23 @@
 package com.test.uberforhotels;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HotelShowAddedRoomActivity extends AppCompatActivity {
 
@@ -19,12 +28,40 @@ public class HotelShowAddedRoomActivity extends AppCompatActivity {
     String[] INTERNET = {};
     String[] ROOM_RENT = {};
 
+    Button button;
+
+    DatabaseReference reff;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel_show_added_room);
 
         listView = findViewById(R.id.listView);
+        button = findViewById(R.id.loadRooms);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reff = (DatabaseReference) FirebaseDatabase.getInstance().getReference().orderByChild("rooms").equalTo("hotelEmail");
+                reff.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String roomNumber = dataSnapshot.child("roomNumber").getValue().toString();
+                        String roomRent = dataSnapshot.child("roomRent").getValue().toString();
+                        String numberOfBeds = dataSnapshot.child("numberOfBeds").getValue().toString();
+                        String internetAvailability = dataSnapshot.child("internetAvailability").getValue().toString();
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
 
         CustomAdapter customAdapter = new CustomAdapter();
 
